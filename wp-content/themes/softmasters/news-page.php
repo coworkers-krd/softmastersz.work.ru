bf6fe607556b7c57b59a91c656b636392f831178
Unknown <benladen339@yandex.ru>
2018-09-08 22:01:21 +0300

    Пагинация настроена
    чтобы проверить вывод кнопок, можно уменьшить параметр  'posts_per_page' на странице news-page.php

--- wp-content/themes/softmasters/news-page.php
+++ wp-content/themes/softmasters/news-page.php
@@ -16,15 +16,37 @@
     <div class="container">
 
     <?php
-    global $post;
+    /*global $post;
     $args = array( 'numberposts' => -1 ,'orderby' => 'date');
     $myposts = get_posts( $args );
     foreach( $myposts as $post ){ setup_postdata($post);
 
         get_template_part("modules/content/news-item");
     }
-    wp_reset_postdata();
+    wp_reset_postdata();*/
+
+    $current_page = (get_query_var('paged')) ? get_query_var('paged') : 1;
+    $params = array(
+        'posts_per_page' => 10, // количество постов на странице
+        'post_type'       => 'post', // тип постов
+        'paged'           => $current_page // текущая страница
+    );
+    query_posts($params);
+
+    $wp_query->is_archive = true;
+    $wp_query->is_home = false;
+
+    while(have_posts()): the_post();
+
+        get_template_part("modules/content/news-item");
+
+    endwhile;
+
+
     ?>
+
+    <? wp_pagenavi(); ?>
+
 
 
     </div>
