38d668b5bbe32d75bf99d6290babc89c2d8ce664
Unknown <benladen339@yandex.ru>
2018-09-08 20:30:43 +0300

    База и отступы.
    Добавил отступ, когда админ вошел админ-бар теперь не перекрывает меню

--- wp-content/themes/cremlin/gallery-slider.php
+++
@@ -1,9 +0,0 @@
-<ul class="project__gallery">
-    <?php
-    foreach ($images as $image) {
-        $src = wp_get_attachment_url($image->ID); // ссылка на изображение
-        $alt = get_post_meta($image->ID, '_wp_attachment_image_alt', true); // атрибут alt
-        ?>
-        <li><img src="<?php echo $src; ?>" alt="<?php echo $alt; ?>" /></li>
-    <?php } ?>
-</ul>
--- wp-content/themes/cremlin/searchform.php
+++
@@ -1,5 +0,0 @@
-<form class="search" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <input type="text" class="search__input" value="" name="s" id="s" placeholder="Найти...">
-    <input  class="search__btn" type="submit" id="searchsubmit" value="" />
-</form>
-
